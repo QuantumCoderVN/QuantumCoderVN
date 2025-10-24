eddf7a2fba02fe86c9569f2d066fe9ccf5f9d9ac
QuantumCoderVN <141411395+QuantumCoderVN@users.noreply.github.com>
2025-10-24 21:46:25 +0000

    Deploying to gh-pages from @ QuantumCoderVN/QuantumCoderVN.github.io@db919903a91847a20fc67b44c05cb93f15d0d823 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "QuantumCoderVN/QuantumCoderVN",
+    "data-repo": "QuantumCoderVN/QuantumCoderVN.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
